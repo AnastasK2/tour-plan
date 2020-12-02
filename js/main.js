1a5f99141a68e61fe18668cfae18813740a772fd
AnastasK2 <ZaporozhetsAnastasia12@gmail.com>
2020-12-02 22:52:24 +0300

    fix: media for footer section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+var menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click', function () {
+  console.log(
+    "Клик по кнопке меню"
+  );
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+});
+
 var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
@@ -29,36 +38,37 @@ var reviewsSlider = new Swiper('.reviews-slider', {
   },
 });
 
-ymaps.ready(init);
-function init(){
-   var myMap = new ymaps.Map("map", {
-    center: [7.890703, 98.294772],
-     zoom: 17
-   });
-   // первый вариант создания геометки
-    // myGeoObject = new ymaps.GeoObject({
-    //    // Описание геометрии.
-    //    geometry: {
-    //      type: "Point",
-    //      coordinates: [7.890703, 98.294772]
-    //     },
-    //     properties: {
-    //       // Контент метки.
-    //       iconContent: 'Hilton Phuket',
-    //       hintContent: 'DoubleTree by Hilton Phuket Banthai Resort'
-    //   }
-    // }, {
-    //    // Опции.
-    //    // Иконка метки будет растягиваться под размер ее содержимого.
-    //   preset: 'islands#blackStretchyIcon',
-    //  });
-    //  myMap.geoObjects.add(myGeoObject);
-    // второй вариант создания геометки
-     myMap.geoObjects.add(new ymaps.Placemark([7.890703, 98.294772], {
-      balloonContent: '<strong>DoubleTree by Hilton Phuket Banthai Resort</strong>',
-      hintContent: 'DoubleTree by Hilton Phuket'
-    }, {
-      preset: 'islands#dotIcon',
-      iconColor: '#735184',
-    }));
-}
+// ymaps.ready(init);
+// function init(){
+//    var myMap = new ymaps.Map("map", {
+//     center: [7.890703, 98.294772],
+//      zoom: 17
+//    });
+//    // первый вариант создания геометки
+//     // myGeoObject = new ymaps.GeoObject({
+//     //    // Описание геометрии.
+//     //    geometry: {
+//     //      type: "Point",
+//     //      coordinates: [7.890703, 98.294772]
+//     //     },
+//     //     properties: {
+//     //       // Контент метки.
+//     //       iconContent: 'Hilton Phuket',
+//     //       hintContent: 'DoubleTree by Hilton Phuket Banthai Resort'
+//     //   }
+//     // }, {
+//     //    // Опции.
+//     //    // Иконка метки будет растягиваться под размер ее содержимого.
+//     //   preset: 'islands#blackStretchyIcon',
+//     //  });
+//     //  myMap.geoObjects.add(myGeoObject);
+//     // второй вариант создания геометки
+//      myMap.geoObjects.add(new ymaps.Placemark([7.890703, 98.294772], {
+//       balloonContent: '<strong>DoubleTree by Hilton Phuket Banthai Resort</strong>',
+//       hintContent: 'DoubleTree by Hilton Phuket'
+//     }, {
+//       preset: 'islands#dotIcon',
+//       iconColor: '#735184',
+//     }));
+// }
+
