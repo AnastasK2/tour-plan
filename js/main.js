0c8fbbae878e88fc34080501fafe0dd864ff7697
AnastasK2 <ZaporozhetsAnastasia12@gmail.com>
2020-12-05 01:52:13 +0300

    create: added modal window

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var menuButton = document.querySelector('.menu-button');
+$(document).ready(function () {
+  var menuButton = document.querySelector('.menu-button');
 
 menuButton.addEventListener('click', function () {
   console.log(
@@ -6,7 +7,7 @@ menuButton.addEventListener('click', function () {
   );
   document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
 });
-
+//слайдер с фото отеля
 var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
@@ -22,7 +23,7 @@ var hotelSlider = new Swiper('.hotel-slider', {
     onlyInViewport: true,
   },
 });
-
+//слайдер с отзывами
 var reviewsSlider = new Swiper('.reviews-slider', {
   // Optional parameters
   loop: true,
@@ -37,7 +38,7 @@ var reviewsSlider = new Swiper('.reviews-slider', {
     onlyInViewport: true,
   },
 });
-
+//яндекс карта
 // ymaps.ready(init);
 // function init(){
 //    var myMap = new ymaps.Map("map", {
@@ -72,3 +73,22 @@ var reviewsSlider = new Swiper('.reviews-slider', {
 //     }));
 // }
 
+//модальное окно
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+  function openModal (){
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+  function closeModal (event){
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+});
