b48fb2147d437126499b264454df592df596db2b
AnastasK2 <ZaporozhetsAnastasia12@gmail.com>
2020-12-09 14:49:56 +0300

    optimisation

--- js/main.js
+++ js/main.js
@@ -38,41 +38,6 @@ var reviewsSlider = new Swiper('.reviews-slider', {
     onlyInViewport: true,
   },
 });
-//яндекс карта
-// ymaps.ready(init);
-// function init(){
-//    var myMap = new ymaps.Map("map", {
-//     center: [7.890703, 98.294772],
-//      zoom: 17
-//    });
-//    // первый вариант создания геометки
-//     // myGeoObject = new ymaps.GeoObject({
-//     //    // Описание геометрии.
-//     //    geometry: {
-//     //      type: "Point",
-//     //      coordinates: [7.890703, 98.294772]
-//     //     },
-//     //     properties: {
-//     //       // Контент метки.
-//     //       iconContent: 'Hilton Phuket',
-//     //       hintContent: 'DoubleTree by Hilton Phuket Banthai Resort'
-//     //   }
-//     // }, {
-//     //    // Опции.
-//     //    // Иконка метки будет растягиваться под размер ее содержимого.
-//     //   preset: 'islands#blackStretchyIcon',
-//     //  });
-//     //  myMap.geoObjects.add(myGeoObject);
-//     // второй вариант создания геометки
-//      myMap.geoObjects.add(new ymaps.Placemark([7.890703, 98.294772], {
-//       balloonContent: '<strong>DoubleTree by Hilton Phuket Banthai Resort</strong>',
-//       hintContent: 'DoubleTree by Hilton Phuket'
-//     }, {
-//       preset: 'islands#dotIcon',
-//       iconColor: '#735184',
-//     }));
-// }
-
 //модальное окно
   var modalButton = $('[data-toggle=modal]');
   var closeModalButton = $('.modal__close');
